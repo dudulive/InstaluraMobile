b8d584831807ba725a2bbafd58e5a16c9bf4a162
dudulive <edurodriguesbraz@hotmail.com>
2019-02-20 20:56:29 -0300

    Melhorando o layout dos posts

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
  */
 
 import React, {Component} from 'react';
-import { Text, View, FlatList, Dimensions, Image} from 'react-native';
+import { Text, View, FlatList, Dimensions, Image, StyleSheet} from 'react-native';
 
 const width = Dimensions.get('screen').width;
 
@@ -21,17 +21,42 @@ export default class App extends Component {
         ];
 
         return (
-            <FlatList style={{marginTop: 20}}
-                keyExtractor={item => String(item.id)}
+            <FlatList style={styles.container}
+                keyExtractor={item => item.id}
                 data={fotos}
                 renderItem={ ({item}) =>
                     <View>
-                        <Text>{item.usuario}</Text>
+                        <View style={styles.cabecalho}>
+                            <Image source={require('./resources/img/alura.jpg')}
+                                    style={styles.fotoDePerfil} />
+                            <Text>{item.usuario}</Text>
+                        </View>
                         <Image source={require('./resources/img/alura.jpg')}
-                            style={{width: width, height: width}} />        
+                                style={styles.foto} />        
                     </View>
                 }
             />
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 20,
+    },
+    cabecalho: {
+        margin: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    fotoDePerfil: {
+        marginRight: 10,
+        borderRadius: 20,
+        width: 40,
+        height: 40,
+    },
+    foto: {
+        width: width,
+        height: width,
+    },
+});
